6829d10bc79bdd06021de93d5338af9935af96b5
hdf <devisangam262@gmail.com>
2025-02-06 16:29:38 +0530

    Update support.js

--- commands/support.js
+++ commands/support.js
@@ -9,8 +9,8 @@ module.exports = {
     options: [],
     run: async (client, interaction, lang) => {
         try {
-            const supportServerLink = "https://discord.gg/xQF9f9yUEM";
-            const githubLink = "https://github.com/GlaceYT";
+            const supportServerLink = "https://discord.gg/E8pbc9EX2f";
+            const githubLink = "https://discord.hazardousdefenseforce.pages.dev/";
             const replitLink = "https://replit.com/@GlaceYT";
             const youtubeLink = "https://www.youtube.com/@GlaceYT";
 
@@ -23,9 +23,9 @@ module.exports = {
                 })
                 .setDescription(lang.support.embed.description
                     .replace("{supportServerLink}", supportServerLink)
-                    .replace("{githubLink}", githubLink)
-                    .replace("{replitLink}", replitLink)
-                    .replace("{youtubeLink}", youtubeLink)
+                    .replace("{githubLink}", SupportServer2)
+                    .replace("{replitLink}", youtubeLink)
+                    .replace("{youtubeLink}", youtubeLink2)
                 )
                 .setImage('https://cdn.discordapp.com/attachments/1113800537402527903/1236803979996958740/11.png?ex=663956f7&is=66380577&hm=3b3c19a11adcb979517a133f2907f671305d23f1f5092cf7df043e6d5cab07bc&')
                 .setTimestamp();
