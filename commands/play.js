c5369c47902076aa46c38328a9e56cd88bb09359
hdf <devisangam262@gmail.com>
2025-02-06 19:48:20 +0530

    Update play.js

--- commands/play.js
+++ commands/play.js
@@ -99,7 +99,56 @@ async function play(client, interaction, lang) {
                 iconURL: musicIcons.beats2Icon,
                 url: config.SupportServer
             })
-            .setDescription(lang.play.embed.successProcessed);
+            .setDescription(lang.play.embed.successProcessed)
+             const { EmbedBuilder } = require('discord.js');
+const config = require("../config.js");
+const musicIcons = require('../UI/icons/musicicons.js');
+
+module.exports = {
+    name: "support",
+    description: "Get support server link",
+    permissions: "0x0000000000000800",
+    options: [],
+    run: async (client, interaction, lang) => {
+        try {
+            const supportServerLink = "https://discord.gg/E8pbc9EX2f";
+            const supportServerLink2 = "https://discord.hazardousdefenseforce.pages.dev/";
+            const youtubeLink = "https://www.youtube.com/channel/UCKxL_CqFtQ98Hmq3zoNtrNw";
+            const youtubeLink2 = "http://www.youtube.com/@LeftDC-33";
+
+            const embed = new EmbedBuilder()
+                .setColor('#b300ff')
+                .setAuthor({
+                    name: lang.support.embed.authorName,
+                    iconURL: musicIcons.beats2Icon, 
+                    url: supportServerLink
+                })
+                .setDescription(lang.support.embed.description
+                    .replace("{supportServerLink}", supportServerLink)
+                    .replace("{supportServer2}", supportServerLink2)
+                    .replace("{youtubeLink}", youtubeLink)
+                    .replace("{youtubeLink2}", youtubeLink2)
+                )
+                .setImage('https://i.imgur.com/ZK30wxa.png')
+                .setTimestamp();
+
+            await interaction.reply({ embeds: [embed] });
+        } catch (e) {
+            console.error(e);
+            const errorEmbed = new EmbedBuilder()
+                .setColor('#ff0000')
+                .setAuthor({
+                    name: lang.support.embed.error,
+                    iconURL: musicIcons.alertIcon,
+                    url: config.SupportServer
+                })
+                .setDescription(lang.support.embed.errorDescription)
+                .setFooter({ text: lang.footer, iconURL: musicIcons.heartIcon });
+
+            await interaction.reply({ embeds: [errorEmbed], ephemeral: true });
+        }
+    },
+};
 
         await interaction.followUp({ embeds: [randomEmbed] });
 
