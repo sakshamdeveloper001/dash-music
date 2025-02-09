6320a3581dd288cc48c5eb9572d3a3d6acbac19d
hdf <devisangam262@gmail.com>
2025-02-09 11:02:22 +0530

    Update config.js

--- config.js
+++ config.js
@@ -22,9 +22,72 @@ module.exports = {
       secure: false
     },
     {
+      name: "ChalresNaig Node",
+      password: "NAIGLAVA-dash.techbyte.host",
+      host: "lavahatry4.techbyte.host",
+      port: 3000,
+      secure: false
+    },
+         {
+      name: "Koi Node V4",
+      password: "prplmoe.me",
+      host: "lavav4.prplmoe.me",
+      port: 1118,
+      secure: false
+    },
+         {
       name: "lavalink.clxud.dev",
       password: "youshallnotpass",
       host: "lavalink.clxud.dev",
+      port: 2333,
+      secure: false
+    },
+         {
+      name: "lavalinkv4.serenetia.com",
+      password: "https://dsc.gg/ajidevserve",
+      host: "lavalinkv4.serenetia.com",
+      port: 80,
+      secure: false
+    },
+    {
+      name: "lavalink.jirayu.net",
+      password: "youshallnotpass",
+      host: "lavalink.jirayu.net",
+      port: 13592,
+      secure: false
+    },
+    {
+      name: "lavahatry4.techbyte.host",
+      password: "NAIGLAVA-dash.techbyte.host",
+      host: "lavahatry4.techbyte.host",
+      port: 3000,
+      secure: false
+    },
+    {
+      name: "lavalink.ipv4.oreshi.com",
+      password: "mfkdlgmklmlenrfmklsdmnfkldnslkansdlknfdlksnflksdnkf",
+      host: "lavalink.ipv4.oreshi.com",
+      port: 2333,
+      secure: false
+    },
+    {
+      name: "lavalink.anantix.network",
+      password: "pg6|(}7fuD_:7d#QQq?9",
+      host: "lavalink.anantix.network",
+      port: 2335,
+      secure: false
+    },
+    {
+      name: "lava-v4.nextgencoders.xyz",
+      password: "freemenahimilta",
+      host: "lava-v4.nextgencoders.xyz",
+      port: 2333,
+      secure: false
+    },
+    {
+      name: "lavalink.alya-project.me",
+      password: "https://alya-project.me/",
+      host: "lavalink.alya-project.me",
       port: 2333,
       secure: false
     }
